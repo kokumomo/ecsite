07c148927f86947610caff8618a35b023166df77
{km} <{k3042227625n@gmail.com}>
2024-03-06 06:48:44 +0900

    edit, update

--- app/Http/Controllers/Owner/ImageController.php
+++ app/Http/Controllers/Owner/ImageController.php
@@ -72,19 +72,12 @@ class ImageController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
      * Show the form for editing the specified resource.
      */
     public function edit(string $id)
     {
-        //
+        $image = Image::findOrFail($id);
+        return view('owner.images.edit', compact('image'));
     }
 
     /**
@@ -92,8 +85,21 @@ class ImageController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $request->validate([
+            'title' => 'string|max:50',
+        ]);
+
+        $image = Image::findOrFail($id);
+            $image->title = $request->title;
+            $image->save();
+            
+            return redirect()
+            ->route('owner.images.index')
+            ->with(['message' => '商品情報を更新しました。', 
+            'status' => 'info']);
     }
+
+    
 
     /**
      * Remove the specified resource from storage.
