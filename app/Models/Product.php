c8cb57122f66baa06550d8527dfc8b93b8f4a29b
{km} <{k3042227625n@gmail.com}>
2024-03-17 06:43:13 +0900

    product edit

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,18 +9,17 @@ use App\Models\SecondaryCategory;
 use App\Models\Image;
 use App\Models\Stock;
 
-
 class Product extends Model
 {
     use HasFactory;
 
     protected $fillable = [
+        'shop_id',
         'name',
         'information',
         'price',
         'is_selling',
         'sort_order',
-        'shop_id',
         'secondary_category_id',
         'image1',
         'image2',
@@ -30,19 +29,36 @@ class Product extends Model
 
     public function shop()
     {
-    return $this->belongsTo(Shop::class);
+        return $this->belongsTo(Shop::class);
     }
 
-    public function category(){
+    public function category()
+    {
         return $this->belongsTo(SecondaryCategory::class, 'secondary_category_id');
     }
 
-    public function imageFirst(){
+    public function imageFirst()
+    {
         return $this->belongsTo(Image::class, 'image1', 'id');
+    }
+
+    public function imageSecond()
+    {
+        return $this->belongsTo(Image::class, 'image2', 'id');
+    }
+
+    public function imageThird()
+    {
+        return $this->belongsTo(Image::class, 'image3', 'id');
+    }
+
+    public function imageFourth()
+    {
+        return $this->belongsTo(Image::class, 'image4', 'id');
     }
 
     public function stock()
     {
-    return $this->hasMany(Stock::class);
+        return $this->hasMany(Stock::class);
     }
 }
