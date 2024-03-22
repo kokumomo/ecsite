74dec422243443040971a953ca7c668757c895cd
{km} <{k3042227625n@gmail.com}>
2024-03-22 20:40:36 +0900

    shop情報

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -119,6 +119,27 @@
         </div>
     </div>
 
+    <div class="modal micromodal-slide" id="modal-1" aria-hidden="true">
+    <div class="modal__overlay" tabindex="-1" data-micromodal-close>
+      <div class="modal__container" role="dialog" aria-modal="true" aria-labelledby="modal-1-title">
+        <header class="modal__header">
+          <h2 class="text-xl text-gray-700" id="modal-1-title">
+            {{ $product->shop->name }}
+          </h2>
+          <button type="button" class="modal__close" aria-label="Close modal" data-micromodal-close></button>
+        </header>
+        <main class="modal__content" id="modal-1-content">
+          <p>
+            {{ $product->shop->information}}
+          </p>
+        </main>
+        <footer class="modal__footer">
+          <button type="button" class="modal__btn" data-micromodal-close aria-label="Close this dialog window">閉じる</button>
+        </footer>
+      </div>
+    </div>
+  </div>
+
     <!-- Scripts -->
     @vite(['resources/css/swiper.css', 'resources/js/swiper.js'])
     <!-- <script src="{{ asset('js/swiper.js')}}"></script> -->
